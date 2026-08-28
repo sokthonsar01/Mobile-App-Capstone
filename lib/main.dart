8a16800ccfafc4218c209d91aa6009dfc333e46e
Your Name <your.email@example.com>
2026-08-28 13:44:40 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 // ===== TEMP FOR TESTING - REMOVE BEFORE COMMITTING =====
 // import 'features/splash/presentation/splash_screen.dart';
 import 'dev_menu_screen.dart';
-// =======================================================
 
 void main() {
   runApp(const MyApp());
